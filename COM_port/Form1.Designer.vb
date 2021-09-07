576e381055131562112b49b85ed993d96ca1f7bf
Aleksandr <anbngm@gmail.com>
2021-09-07 08:55:56 +0500

    Add files via upload

---
+++ COM_port/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(12, 173)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(356, 27)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Передать данные в COM порт DTR (4) - BG (5)"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(240, 16)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Номер СОМ порта, например: COM1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(268, 40)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "COM13"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(291, 32)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Пауза в миллисекундах между выводом" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "положительных и отрицательных символов"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(317, 86)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(51, 22)
+        Me.TextBox2.TabIndex = 2
+        Me.TextBox2.Text = "100"
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.Label3.Location = New System.Drawing.Point(12, 123)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(236, 32)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Общее количество положительных" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "и отрицательных символов (0 + 1)"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.TextBox3.Location = New System.Drawing.Point(317, 131)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(51, 22)
+        Me.TextBox3.TabIndex = 2
+        Me.TextBox3.Text = "5"
+        Me.TextBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.CheckBox1.Location = New System.Drawing.Point(15, 12)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(150, 20)
+        Me.CheckBox1.TabIndex = 4
+        Me.CheckBox1.Text = "Отображать время"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.ClientSize = New System.Drawing.Size(381, 214)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "COM_Write()"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+End Class
---
+++ COM_port/Form1.vb
@@ -0,0 +1,63 @@
+﻿Imports System.IO.Ports
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Dim COM1 As IO.Ports.SerialPort = Nothing
+        Dim COM1 As SerialPort
+        Dim i, z, NumSym As Integer
+        Dim s1 As String
+        NumSym = CInt(TextBox3.Text)
+        z = 1
+
+        If CheckBox1.Checked = True Then
+            Form2.Show()
+            Form2.TextBox1.Text = ""
+            Try
+                COM1 = My.Computer.Ports.OpenSerialPort(TextBox1.Text)
+                For i = 1 To NumSym
+                    COM1.DtrEnable = True
+                    s1 = Form2.TextBox1.Text
+                    s1 = s1 + CStr(z) + ". " + " True  " + TimeString
+                    z += 1
+                    s1 = s1 + Chr(13) + Chr(10)
+                    Form2.TextBox1.Text = CStr(s1)
+                    Threading.Thread.Sleep(CInt(TextBox2.Text))
+
+                    COM1.DtrEnable = False
+                    s1 = Form2.TextBox1.Text
+                    s1 = s1 + CStr(z) + ". " + " False  " + TimeString
+                    z += 1
+                    s1 = s1 + Chr(13) + Chr(10)
+                    Form2.TextBox1.Text = CStr(s1)
+                    Threading.Thread.Sleep(CInt(TextBox2.Text))
+                Next i
+                COM1.Close()
+            Catch ex As Exception
+                MsgBox("Что-то пошло не так, прога ушла в исключение :-(", vbCritical)
+                GoTo lb1
+            End Try
+            MsgBox("Прога выполнена!", vbInformation)
+lb1:
+        Else
+            Form2.TextBox1.Text = ""
+            Form2.Visible = False
+            Try
+                COM1 = My.Computer.Ports.OpenSerialPort(TextBox1.Text)
+
+                For i = 1 To NumSym
+                    COM1.DtrEnable = True
+                    Threading.Thread.Sleep(CInt(TextBox2.Text))
+
+                    COM1.DtrEnable = False
+                    Threading.Thread.Sleep(CInt(TextBox2.Text))
+                Next i
+                COM1.Close()
+            Catch ex As Exception
+                MsgBox("Что-то пошло не так, прога ушла в исключение :-(", vbCritical)
+                GoTo lb2
+            End Try
+            MsgBox("Прога выполнена!", vbInformation)
+lb2:
+        End If
+    End Sub
+
+End Class
